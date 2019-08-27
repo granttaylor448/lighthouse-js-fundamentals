4b75fcfc9b2306c4a6f14ca5cd0b25f8b5188074
granttaylor448 <granttaylor448@gmail.com>
2019-08-27 02:23:02 +0000

    Iterating Over Arrays: Banana Bread

---
+++ ingredients.js
@@ -0,0 +1,19 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+for (let i=0; i< ingredients.length; i++){
+  console.log(ingredients[i]);
+}
+
+let i=0
+while (i < ingredients.length){
+  console.log(ingredients[i]);
+  i ++
+}
+for (let i=7; i >=0; i--){
+  console.log(ingredients[i]);
+}
+
+//const sorted=(function(ingredients.reverse))
+//for (let i=0; i< ingredients.length; i++){
+//  console.log(ingredients[i]);
+
